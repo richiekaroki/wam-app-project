4a4ba2217b9b4e5b76032c0698a36c6ec9b727cb
Richard Karoki <karokirichard522@gmail.com>
2024-11-05 20:51:30 +0300

    Create HomePage.vue

---
+++ wam-app/src/views/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <HeroSection />
+    
+  </div>
+</template>
+
+<script>
+import HeroSection from '../components/HeroSection.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    HeroSection,
+  },
+};
+</script>
+
+<style scoped>
+/* Smooth scrolling for the entire page */
+html {
+  scroll-behavior: smooth;
+}
+
+/* Additional styles for the homepage */
+.other-sections {
+  background-color: #f7f7f7; /* Light background for contrast */
+}
+</style>
