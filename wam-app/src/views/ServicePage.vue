f69c9ce9a9948e5a0451dc4777546289433fc39d
Richard Karoki <karokirichard522@gmail.com>
2024-11-05 20:51:19 +0300

    Create ServicePage.vue

---
+++ wam-app/src/views/ServicePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Services Page</h1>
+    <p>Details about services offered.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ServicePage',
+}
+</script>
