91ba2f134d40c5cf16c24aa1aae789b8137c81f7
Richard Karoki <karokirichard522@gmail.com>
2024-11-05 19:07:10 +0300

    Create App.vue

---
+++ wam-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NavbarComponent />
+    <router-view />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import FooterComponent from './components/FooterComponent.vue'
+import NavbarComponent from './components/NavbarComponent.vue'
+
+export default {
+  components: {
+    NavbarComponent,
+    FooterComponent,
+  },
+}
+</script>
+
+<style></style>
