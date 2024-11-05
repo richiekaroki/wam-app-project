1b04f89e9c501952148f6304642f854165963249
Richard Karoki <karokirichard522@gmail.com>
2024-11-05 23:51:31 +0300

    Create NavLink.vue

---
+++ wam-app/src/components/NavLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <router-link :to="to" class="hover:text-highlight" aria-label="Navigate to {{ label }}">
+      {{ label }}
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'NavLink',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
