ae8a6a9394230e81e47a1e3bc9e955526c52690a
Richard Karoki <karokirichard522@gmail.com>
2024-11-08 23:18:04 +0300

    Update HomePage.vue

--- wam-app/src/views/HomePage.vue
+++ wam-app/src/views/HomePage.vue
@@ -1,19 +1,30 @@
 <template>
   <div>
     <HeroSection />
-    
+    <ServiceSection />
+    <FeaturedWork />
+    <TestimonialSection />
+    <ContactForm />
   </div>
 </template>
 
 <script>
-import HeroSection from '../components/HeroSection.vue';
+import FeaturedWork from '../components/FeaturedWork.vue'
+import HeroSection from '../components/HeroSection.vue'
+import ServiceSection from '../components/ServiceSection.vue'
+import TestimonialSection from '../components/TestimonialSection.vue'
+import ContactForm from '../components/ContactForm.vue'
 
 export default {
   name: 'HomePage',
   components: {
     HeroSection,
+    ServiceSection,
+    FeaturedWork,
+    TestimonialSection,
+    ContactForm,
   },
-};
+}
 </script>
 
 <style scoped>
@@ -24,6 +35,6 @@ html {
 
 /* Additional styles for the homepage */
 .other-sections {
-  background-color: #f7f7f7; /* Light background for contrast */
+  background-color: accent; /* Light background for contrast */
 }
 </style>
