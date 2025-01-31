86e10ce6ca6582769269e35a378132d0ddf9e03a
Richard Karoki <karokirichard522@gmail.com>
2025-01-31 22:45:22 +0300

    Update App.vue

--- wam-app/src/App.vue
+++ wam-app/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
+  <div id="app">
     <NavbarComponent />
-    <router-view />
+    <main>
+      <router-view />
+    </main>
     <FooterComponent />
   </div>
 </template>
@@ -11,6 +13,7 @@ import FooterComponent from './components/FooterComponent.vue'
 import NavbarComponent from './components/NavbarComponent.vue'
 
 export default {
+  name: 'App',
   components: {
     NavbarComponent,
     FooterComponent,
@@ -18,4 +21,35 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+/* CSS Reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  flex: 1;
+  padding: 2rem;
+}
+
+/* Responsive Design */
+@media (max-width: 768px) {
+  main {
+    padding: 1rem;
+  }
+}
+
+@media (max-width: 480px) {
+  main {
+    padding: 0.5rem;
+  }
+}
+</style>
